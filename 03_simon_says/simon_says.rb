1468737eb7e1738ba539fe7a77d28aed4a8f1b3d
david-dacruz <dvddcrz@gmail.com>
2018-01-16 17:36:23 +0100

    simon says titleize function missing

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,24 @@
 #write your code here
+def echo(greeting)
+  greeting
+end
+
+def shout(str)
+  str.upcase
+end
+
+def repeat(str, number = 2)
+  ([str] * number).join(" ")
+end
+
+def start_of_word(str, n = 0)
+  str.slice(0, n)
+end
+
+def first_word(str)
+  str.rpartition(" ").first
+end
+
+def titleize(str)
+ 
+end
