210dea4becdde42f5d9443f45267ad9c693861c7
david-dacruz <dvddcrz@gmail.com>
2018-01-16 11:27:45 +0100

    temperarture.rb push

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+def ftoc(degrees_f)
+ degrees_c = degrees_f - 32 
+ degrees_c = degrees_c * 5 
+ degrees_c = degrees_c / 9
+end
+
+def ctof(degrees_c)
+  degrees_f = degrees_c * 1.8 + 32
+end
